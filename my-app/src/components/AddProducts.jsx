26d60134102c0d01caa3cf8a7d46319ac35b5fdd
balja0803 <balja0803@gmail.com>
2023-03-10 12:28:55 +0800

    test

--- my-app/src/components/AddProducts.jsx
+++ my-app/src/components/AddProducts.jsx
@@ -1,5 +1,92 @@
 import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
 
 export default function AddProducts() {
-  return <div>add products</div>;
+  const [brands, setBrands] = useState([]);
+  const [categories, setCategory] = useState([]);
+
+  async function fetchOptions() {
+    let response = await axios.get("http://localhost:4040/brands");
+    let catResponse = await axios.get("http://localhost:4040/categories");
+    setBrands(response.data);
+    setCategory(catResponse.data);
+  }
+
+  useEffect(() => {
+    fetchOptions();
+  }, []);
+
+  return (
+    <div>
+      <Form>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">name</label>
+          <input
+            type=""
+            className="form-control"
+            id="exampleFormControlInput1"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput2">price</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput2"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput3">Image</label>
+          <input
+            type="file"
+            className="form-control"
+            id="exampleFormControlInput3"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput4">Stock</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput4"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput5">Sale</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput5"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlSelect1">category</label>
+          <select className="form-control" id="exampleFormControlSelect1">
+            {categories &&
+              categories.map((category, i) => (
+                <option key={i}> {category.name} </option>
+              ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlSelect2">brand</label>
+          <select className="form-control" id="exampleFormControlSelect2">
+            {brands &&
+              brands.map((brand, i) => <option key={i}>{brand.name} </option>)}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlTextarea1">description</label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <Button type="Submit">Add Product</Button>
+      </Form>
+    </div>
+  );
 }
--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -1,3 +1,56 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import Table from "react-bootstrap/Table";
+
 export default function Products() {
-  return <div>all products</div>;
+  const [products, setProducts] = useState([]);
+
+  async function fetchData() {
+    let response = await axios.get("http://localhost:4040/products");
+    let prods = await response.data;
+    setProducts(prods);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {products && (
+        <Table className="table">
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">name</th>
+              <th scope="col">price</th>
+              <th scope="col">sale</th>
+              <th scope="col">stock</th>
+              <th scope="col">brand</th>
+              <th scope="col">category</th>
+              <th scope="col">description</th>
+              <th scope="col">date</th>
+              <th scope="col">options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <tr key={index}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.sale}</td>
+                <td>{product.stock}</td>
+                <td>{product.brand}</td>
+                <td>{product.category}</td>
+                <td>{product.description}</td>
+                <td>{product.created_date.slice(0, 10)}</td>
+                <td>edit/options</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
 }
